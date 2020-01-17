efbde3d40e7b232f62292cfbbc8f78ab80bc5aea
Dustin <clerin+gitkraken@gmail.com>
2020-01-17 15:14:20 -0800

    added paddle and ball collision detection.

--- js/game.js
+++ js/game.js
@@ -13,23 +13,19 @@ export default class Game {
     }
 
     init() {
-        let paddle = new Paddle(this);
-        let ball = new Ball(this);
+        this.paddle = new Paddle(this);
+        this.ball = new Ball(this);
 
-        this.gameObjects = [ball, paddle];
+        this.gameObjects = [this.ball, this.paddle];
 
-        new InputHandler(paddle);
+        new InputHandler(this.paddle);
     }
 
     update(deltaTime) {
-        // this.paddle.update(deltaTime);
-        // this.ball.update(deltaTime);
         this.gameObjects.forEach( obj => obj.update(deltaTime));
     }
 
     draw(ctx) {
-        // this.paddle.draw(ctx);
-        // this.ball.draw(ctx);
         this.gameObjects.forEach( obj => obj.draw(ctx));
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import Game from './game.js';
 
-// paddle.default()
 let canvas = document.querySelector("#game-screen");
 let ctx = canvas.getContext("2d");
 
@@ -10,15 +9,6 @@ const PADDLE_SPEED = 7;
 const BALL_SIZE = 16;
 const BALL_SPEED = 2;
 
-// ctx.clearRect(0, 0, 600, 800);
-/*
-// how to draw on the canvas
-ctx.fillStyle = '#f00';
-ctx.fillRect(20, 20, 100, 100);
-
-ctx.fillStyle = "#ff0";
-ctx.fillRect(140, 20, 100, 100);
-//*/
 let game = new Game(GAME_WIDTH, GAME_HEIGHT, PADDLE_SPEED, BALL_SIZE, BALL_SPEED);
 
 game.init();
@@ -33,15 +23,6 @@ function gameLoop(timestamp) {
 
     game.update(deltaTime);
     game.draw(ctx);
-
-    // paddle.update(deltaTime);
-    // ball.update(deltaTime);
-
-    // paddle.draw(ctx);
-    // ball.draw(ctx);
-
-    // ball.draw(ctx);
-    // ctx.drawImage(imgBall, 10, 10, 16, 16);
 
     requestAnimationFrame(gameLoop);
 }
